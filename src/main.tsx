2a36b8cc0e7549f1a5668c2382253644e51dc0c3
赵建斌 <zjb01237247@alibaba-inc.com>
2022-07-13 18:16:19 +0800

    feature(ui):  semi-ui多语言处理

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,23 @@ import '../mock/index';
 import { ConfigContextProvider } from './utils/configContext';
 import 'animate.css';
 import './index.scss';
+import zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN';
+import en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US';
+import { LocaleProvider } from '@douyinfe/semi-ui';
+import Cookies from 'js-cookie';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ConfigContextProvider>
             <ViewportProvider>
                 <BrowserRouter>
-                    <Layout>
-                        <Router />
-                    </Layout>
+                    <LocaleProvider
+                        locale={Cookies.get('i18next') == 'zh' ? zh_CN : en_US}
+                    >
+                        <Layout>
+                            <Router />
+                        </Layout>
+                    </LocaleProvider>
                 </BrowserRouter>
             </ViewportProvider>
         </ConfigContextProvider>
